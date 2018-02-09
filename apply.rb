46f5affb4436fab00779b64f31a8235c86a4f621
Pushkar Kadam <pushkar.kadam@paydock.com>
2018-02-09 11:28:49 +1100

    A separate function created to convert into hash

--- apply.rb
+++ apply.rb
@@ -1,21 +1,16 @@
 require "json"
 require_relative "game"
 require_relative "monster"
+require_relative "object_to_hash"
 
 gamesphere = Game.new
 	gamesphere.game = "call_of_duty"
 	gamesphere.title = "Advanced_Warfare"
-lil_monster = Monster.new
-	lil_monster.health = "100"
-	lil_monster.xp = "100"
-	monster_hash = Hash.new
-	lil_monster.instance_variables.each {|x| monster_hash[x[1..-1]] = lil_monster.instance_variable_get(x)}
-gamesphere.Monster = monster_hash
+		lil_monster = Monster.new
+			lil_monster.health = "100"
+			lil_monster.xp = "100"
+	gamesphere.Monster = ConvertToHash.object_to_hash(lil_monster)
 
-gamesphere.instance_variables
-properties = Hash.new
-gamesphere.instance_variables.each {|x| properties[x[1..-1]] = gamesphere.instance_variable_get(x)}
-
-properties
-
-properties.to_json
+gamesphere = ConvertToHash.object_to_hash(gamesphere)
+gamesphere
+gamesphere.to_json
---
+++ object_to_hash.rb
@@ -0,0 +1,9 @@
+# require_relative "apply"
+
+class ConvertToHash
+	def self.object_to_hash(object)
+		bigObject = Hash.new
+		object.instance_variables.each {|x| bigObject[x[1..-1]] = object.instance_variable_get(x)}
+		bigObject
+	end
+end
